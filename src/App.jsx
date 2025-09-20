48b5d5bc4693b0925d55589a0b4e8845c3d8abe2
rob587 <robertocamm02@yahoo.com>
2025-09-20 12:31:13 +0200

    [fix] app.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import HomePage from "./pages/HomePage"
 import DetailProduct from "./pages/DetailFilm"
 
 import  DefaultLayout from "./layout/DefaultLayout"
-import  {BrowserRouter, Routes, Route} from "react-router-dom"
+import  {BrowserRouter, Routes, Route, Links} from "react-router-dom"
 
 function App() {
   
 
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route element ={<DefaultLayout/>}>
-          <Route path='/' element={<HomePage/>}></Route>
-          <Route path='/detailproduct' element={<DetailProduct/>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="details" element={<DetailProduct />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
       
     </>
   )
--- src/pages/DetailFilm.jsx
+++ src/pages/DetailFilm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const DetailFilm = () => {
   return (
     <div>
-      
+      sdsd
     </div>
   )
 }
