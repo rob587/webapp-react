03a83af295c9e843ad401e2f7d0fae37543b9b6a
rob587 <robertocamm02@yahoo.com>
2025-09-20 17:35:44 +0200

    axios

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const HomePage = () => {
+
+  const [movies, setMovies] = useState([])
+
+  const url = ('http://localhost:3000/movies')
+
+    const fetchData = () =>{
+    axios.get(url).then((resp)=>{
+      setMovies(resp.data)
+    })
+  }
+
+
   return (
-    <div>
-      ciao
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          {movies.map((movie)=>{
+            return(
+          <div className="col-6">
+
+          </div>
+
+            )
+          })}
+        </div>
+      </div>
+    </>
   )
 }
 
