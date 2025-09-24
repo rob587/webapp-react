ea6c86fd7c86f20cba896f7a510b6456f5ef6eb3
rob587 <robertocamm02@yahoo.com>
2025-09-24 15:13:53 +0200

    loader added

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='overlay'>
+        <div className="spinner-border text-main"></div>
+      
+    </div>
+  )
+}
+
+export default Loader
